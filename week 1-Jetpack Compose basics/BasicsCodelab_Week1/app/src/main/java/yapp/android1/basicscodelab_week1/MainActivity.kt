71db01d993cca630a445dfbbccc3e2784f7822c1
Joseph <wjsdudtkd95@gmail.com>
2021-11-01 19:30:45 +0900

    week1-step1: initialize proeject

---
+++ week 1-Jetpack Compose basics/BasicsCodelab_Week1/app/src/main/java/yapp/android1/basicscodelab_week1/MainActivity.kt
@@ -0,0 +1,46 @@
+package yapp.android1.basicscodelab_week1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import yapp.android1.basicscodelab_week1.ui.theme.BasicsCodelab_Week1Theme
+import yapp.android1.basicscodelab_week1.ui.theme.Shapes
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicsCodelab_Week1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BasicsCodelab_Week1Theme {
+        Greeting("Android")
+    }
+}
