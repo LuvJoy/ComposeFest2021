a36739f8a667c3af47375ad588deb11ab7cd310f
Joseph <wjsdudtkd95@gmail.com>
2021-11-22 16:52:31 +0900

    feat(week2): step6 - Working with Lists

--- week 2-1-Layouts in JetpackCompose/LayoutsInJetpackCompose/app/src/main/java/com/android/layoutinjetpackcompose/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutsInJetpackCompose/app/src/main/java/com/android/layoutinjetpackcompose/MainActivity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import android.view.RoundedCorner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -18,6 +15,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -28,7 +26,9 @@ import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.android.layoutinjetpackcompose.ui.theme.LayoutInJetpackComposeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,7 +37,8 @@ class MainActivity : ComponentActivity() {
             LayoutInJetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    PhotographerCard()
+                    //SimpleList()
+                    LazyList()
                 }
             }
         }
@@ -57,6 +58,15 @@ fun PhotograhperCardPreview() {
 fun LayoutsCodeLabPreview() {
     LayoutInJetpackComposeTheme {
         LayoutsCodelab()
+    }
+}
+
+@Preview
+@Composable
+fun LazyColumnPreview() {
+    LayoutInJetpackComposeTheme {
+        //SimpleList()
+        LazyList()
     }
 }
 
@@ -113,9 +123,72 @@ fun PhotographerCard(modifier: Modifier = Modifier) {
         ) {
             Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
             CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-                Text("3 minutes age", style = MaterialTheme.typography.body2 )
+                Text("3 minutes age", style = MaterialTheme.typography.body2)
             }
         }
     }
 
+}
+
+@Composable
+fun SimpleList() {
+    val scrollState = rememberScrollState()
+
+    Column(Modifier.verticalScroll(scrollState)) {
+        repeat(100) {
+            Text("Item #$it")
+        }
+    }
+}
+
+@Composable
+fun LazyList() {
+    val listSize = 100
+
+    val scrollState = rememberLazyListState()
+
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text(text = "Scroll to the Top")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize -1)
+                }
+            }) {
+                Text(text = "Scroll to the End")
+            }
+        }
+
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(it)
+            }
+        }
+    }
+
+
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+
+        Image(
+            painter = rememberImagePainter(data = "https://developer.android.com/images/brand/Android_Robot.png"),
+            contentDescription = "Android Image",
+            modifier = Modifier.size(50.dp)
+        )
+
+        Spacer(modifier = Modifier.size(10.dp))
+        Text(text = "Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
 }
